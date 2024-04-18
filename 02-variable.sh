25b233b944d1d9a8d637015fc5a5865450d9ef59
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-18 16:10:53 +0530

    new changes

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+echo "Rakesh: : Hello srinath, how r u?"
+echo "Srianth: : Hi Rakesh i'm fine how r u"
+echo "Rakesh: : I am fine too how is ur work"
+echo "Srinath: : not bad, i'm thinking to upgrade decops"
