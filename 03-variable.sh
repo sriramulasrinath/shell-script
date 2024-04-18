dfc29dbf4e4a340fe8bf440b9f1deae7f48e9cc4
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-18 16:18:37 +0530

    new changes

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+PERSON1=Rakesh
+PERSON2=Srinath
+
+
+echo "$PERSON1: : Hello $PERSON2, how r u?"
+echo "$PERSON2: : Hi $PERSON1 i'm fine how r u"
+echo "$PERSON1: : I am fine too how is ur work"
+echo "$PERSON2: : not bad, i'm thinking to upgrade devops"
