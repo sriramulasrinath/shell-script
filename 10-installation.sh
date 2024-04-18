54d31d950e2fd5f2cec9cee9306fa324ade0557d
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-18 19:44:51 +0530

    new changes

---
+++ 10-installation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1
+else
+    echo "You are a SuperUser"
+fi 
+
+dnf install mysql -y
+echo "is script proceeding?"
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql....FAILURE"
+    exit 1
+else
+    echo "Installation of mysql....SUCCESS"
+fi 
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git....FAILURE"
+    exit 1
+else
+    echo "Installation of git....SUCCESS"
+fi
+
+echo "is script proceeding?"
