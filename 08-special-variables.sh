b0793fea2d7b4813276f47f06df0c18520d14883
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-18 17:15:04 +0530

    new changes

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "All variables: :$@"
+echo "Number of variables passed: :$#"
+echo "Script Name: $0"
+echo "Current Working Directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process Id of the current shell script: $$"
