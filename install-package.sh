d38e9e875cb71994c5b3ca99bc5be567c32ba5b2
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-20 12:07:40 +0530

    new changes

--- install-package.sh
+++ install-package.sh
@@ -10,13 +10,19 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 $R FAILURE...$N"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo -e "$2 $G SUCCESS...$N"
+        echo -e "$2...$G SUCCESS $N"
     fi
-
 }
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1
+else
+    echo "You are a Super User."
+fi 
 
 for i in $@
 do
