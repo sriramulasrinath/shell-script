4a4c8a5af9ab32e52cab12452c4722dae328a513
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-20 12:10:02 +0530

    new changes

--- install-package.sh
+++ install-package.sh
@@ -30,9 +30,9 @@ do
     dnf list installed $i &>>LOGFILE
     if [ $? -eq 0 ]
     then
-        echo -e "$i already installed...$Y SKIPPING $N"
+        echo -e "$i already installed...$Y SKIPPING  $N"
     else
         dnf install $i -y $>>LOGFILE
-    VALIDATE $? "Installation of $i"
+        VALIDATE $? "Installation of $i"
     fi
 done
