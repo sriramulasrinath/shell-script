e03675ae440bd466d7679d084ab27cf765c72d69
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-20 11:51:52 +0530

    new changes

---
+++ install-packeages.sh
@@ -0,0 +1,31 @@
+USERID=(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [$1 -ne 0 ]
+    then
+        echo -e "$2 $R FAILURE...$N"
+    else
+        echo -e "$2 $G SUCCESS...$N"
+    fi
+
+}
+
+for i in $@
+do
+    echo "Package to install: $i"
+    dnf list installed $i &>>LOGFILE
+    if [ $? -eq 0]
+    then
+        echo -e "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y $>>LOGFILE
+    VALIDATE $? "Installation of $i"
+    fi
+done
