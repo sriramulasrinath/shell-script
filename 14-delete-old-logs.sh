5d596af78122969560cc572e5f544e645ad9e537
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-25 11:29:24 +0530

    commiting changes

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -13,11 +13,19 @@ else
     echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
     exit 1
 fi
-FILES=$(find $SOURCE_DIRECTORY -name "*.log")
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
-while IFS= zip compressed_file.zip $FILES
-do 
-    echo "compressing files: $FILES"
-    mv $FILES srinath
-done <<< $FILES
+#while IFS= read -r line
+#do 
+#    echo "Deleting file: $line"
+#    rm -rf $line
+#done <<< $FILES
+
+
+
+# Iterate over each file in $FILES
+while IFS= read -r file; do
+    echo "Compressing file: $file"
+    zip compressed_files.zip "$file"
+done <<< "$FILES"
 
