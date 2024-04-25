66038e3b1aa4b968ccbb0beaa4b426e4c8cf94d4
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-25 17:02:34 +0530

    commiting changes

--- 15-disc-usage.sh
+++ 15-disc-usage.sh
@@ -1,12 +1,14 @@
 DISK_USAGE=$(df -hT | grep xfs) 
 DISK_THRESHOLD=10
-
+MESSAGE=""
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line |  awk -F " " '{print $NF}')
     if [ $USAGE -ge  $DISK_THRESHOLD ]
     then 
-        echo "$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE"
+        Message+= "$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE"
     fi
 done  <<< $DISK_USAGE
+
+echo "Message: $MESSAGE
