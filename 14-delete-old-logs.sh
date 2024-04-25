234eeb4c1e10cf851bf857596f6a6f0abf183358
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-25 10:39:48 +0530

    commiting changes

---
+++ 14-delete-old-logs.sh
@@ -0,0 +1,23 @@
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+#d means specified directory exist or not
+then    
+    echo -e "$G Source directory exists $N"
+else    
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+fi
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +10)
+
+while IFS= read -r line
+do 
+    echo "Deleting file: $line"
+    rm -rf $line
+done <<< $FILES
+
